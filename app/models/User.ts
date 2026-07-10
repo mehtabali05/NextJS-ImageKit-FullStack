845d4707ed6037c7ba472c9a0935e5aaf9a0cc23
unknown <mehtabalics7@gmail.com>
2026-07-10 03:57:51 -0700

    add: prehook added for password

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,4 +1,5 @@
-import mongoose from "mongoose";
+import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface IUser{
     email: string;
@@ -7,3 +8,21 @@ export interface IUser{
     createdAt?: Date;
     updatedAt?: Date;
 }
+
+const userSchema = new Schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true  
+    }
+},{timestamps: true});
+
+userSchema.pre("save", async function(){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+});
